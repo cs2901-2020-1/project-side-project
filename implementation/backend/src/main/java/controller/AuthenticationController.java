117ef434e7832e9974a645c26209d708cb05dc39
Carlos Cupe <carlos.cupe@utec.edu.pe>
2020-06-11 12:41:27 -0500

    signin api - teacher and student

--- implementation/backend/src/main/java/controller/AuthenticationController.java
+++ implementation/backend/src/main/java/controller/AuthenticationController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import services.UserService;
+import services.UsuarioService;
 import config.JwtTokenUtil;
 import data.entities.*;
 import data.models.*;
@@ -26,10 +27,33 @@ public class AuthenticationController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private UsuarioService usuarioService;
+
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public ResponseEntity<AuthToken> webLogin(@RequestBody LoginWeb loginUser) throws AuthenticationException {
-        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginUser.getUsername(), loginUser.getPassword()));
-        final Usuario user = userService.findOne(loginUser.getUsername());
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginUser.getEmail(), loginUser.getPassword()));
+        final Usuario user = userService.findOne(loginUser.getEmail());
+        final String token = jwtTokenUtil.generateToken(user);
+        return new ResponseEntity<>(new AuthToken(user.getId().toString(), token), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/student", method = RequestMethod.POST)
+    public ResponseEntity<?> webStudentSignin(@RequestBody StudentSignin student) {
+        if (usuarioService.findOneByEmail(student.getEmail()) != null) {
+            return new ResponseEntity<>("Correo ya existente", HttpStatus.CONFLICT);    
+        }
+        final Usuario user = usuarioService.createStudent(student);
+        final String token = jwtTokenUtil.generateToken(user);
+        return new ResponseEntity<>(new AuthToken(user.getId().toString(), token), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/teacher", method = RequestMethod.POST)
+    public ResponseEntity<?> webTeacherSignin(@RequestBody TeacherSignin teacher) {
+        if (usuarioService.findOneByEmail(teacher.getEmail()) != null) {
+            return new ResponseEntity<>("Correo ya existente", HttpStatus.CONFLICT);    
+        }
+        final Usuario user = usuarioService.createTeacher(teacher);
         final String token = jwtTokenUtil.generateToken(user);
         return new ResponseEntity<>(new AuthToken(user.getId().toString(), token), HttpStatus.OK);
     }
