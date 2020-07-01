9af405b5e1f136869ffa9583051b2a277e03e2af
Angel Motta <angelmotta@gmail.com>
2020-07-01 16:04:30 -0500

    upload content profesor

--- implementation/frontend/src/app/dashboard/teacher-dashboard/teacher-dashboard.component.ts
+++ implementation/frontend/src/app/dashboard/teacher-dashboard/teacher-dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-teacher-dashboard',
@@ -7,9 +9,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeacherDashboardComponent implements OnInit {
 
-  constructor() { }
+  contentUploadForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
+    this.contentUploadForm = this.formBuilder.group({
+      curso: ['', Validators.required],
+      tema: ['', Validators.required],
+      titulo: ['', Validators.required],
+      descripcion: ['', Validators.required],
+      fileVideo: ['', Validators.required],
+      filePdf: ['', Validators.required]
+    });
+  }
+
+  onUpload(){
+    let request = {
+      'curso': this.curso.value,
+      'tema': this.tema.value,
+      'titulo': this.titulo.value,
+      'descripcion': this.descripcion.value
+    }
+  }
+
+  get curso(){
+    return this.contentUploadForm.get('curso')
+  }
+
+  get tema(){
+    return this.contentUploadForm.get('tema')
+  }
+
+  get titulo(){
+    return this.contentUploadForm.get('titulo')
+  }
+
+  get descripcion(){
+    return this.contentUploadForm.get('descripcion')
   }
 
 }
